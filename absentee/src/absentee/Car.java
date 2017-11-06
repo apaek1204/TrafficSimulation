6ce6f43c6921c0bf62229ccd193ce676ace9b971
apaek <apaek@LAPTOP-KJ995O2C>
2017-11-06 17:54:42 -0500

    add curMap

--- absentee/src/absentee/Car.java
+++ absentee/src/absentee/Car.java
@@ -5,19 +5,18 @@ import java.util.Observer;
 
 public abstract class Car extends Observable implements Observer{
 	private double maxVelocity;
-	//private point curPos;
+	private int curX, curY;
 	private double breakDistance;
-	//private point destinations;
-
+	private int desX, desY;
 	public Car(){
 
 	}
-	public void move(){
+	public void move(Map curMap){
 		notifyObservers();
 
 	}
+
 	public void update(Observable car, Object arg1){
 		Car tempcar = (Car) car;
-
 	}
 }
---
+++ absentee/src/absentee/Map.java
@@ -0,0 +1,5 @@
+package absentee;
+
+public class Map {
+
+}
