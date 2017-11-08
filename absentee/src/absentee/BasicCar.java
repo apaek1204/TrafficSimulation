d5be235c2884775e45097d6e9c2cf56d79233f99
apaek <apaek@LAPTOP-KJ995O2C>
2017-11-08 17:22:30 -0500

    update constructor for basic car

--- absentee/src/absentee/BasicCar.java
+++ absentee/src/absentee/BasicCar.java
@@ -4,7 +4,7 @@ import java.awt.Point;
 
 public class BasicCar extends Car{
 
-	public BasicCar(double maxVel, double breakDis, Point cur, Point des, int dir, double s) {
+	public BasicCar(double maxVel, double breakDis, Point cur, Point des, int dir, Point s) {
 		super(maxVel, breakDis, cur, des, dir, s);
 		// TODO Auto-generated constructor stub
 	}
