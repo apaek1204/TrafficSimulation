7dd8ddcf221d72f5971c3605899089d36bf2fabe
quyaoxian <quyaoxian@gmail.com>
2017-11-08 17:26:02 -0500

    modified intersection class, added stoplight class

--- absentee/src/absentee/Intersection.java
+++ absentee/src/absentee/Intersection.java
@@ -3,12 +3,19 @@ package absentee;
 public class Intersection {
 	Car currentCar;
 	Road[] roads;
-	Stoplight s;
+	Stoplight stoplight;
+	public Intersection(Road[] r, Stoplight s) {
+		roads = r;
+		stoplight = s;
+		currentCar = null;
+	}
 	public Boolean canEnter(Car car) {
+		//0 left, 1 right, 2 up, 3 down 
 		int nextDir = car.getNextDirection();
 		if(currentCar != null) {
 			return false;
 		}
+
 		if(s.lights[nextDir] == 0) return false;
 		
 		return true;
