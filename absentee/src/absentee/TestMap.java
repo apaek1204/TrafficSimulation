ae655f52c375c2346cdb6fc39a292132ab244ede
Daschmied <DouglasSchmieder@yahoo.com>
2017-11-10 19:09:51 -0500

    Junit Tests Map/Simulation
    Added simple Junit tests

---
+++ absentee/src/absentee/TestMap.java
@@ -0,0 +1,17 @@
+package absentee;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMap {
+
+	@Test
+	public void test() {
+		Map testMap = Map.getInstance();
+
+		assert(testMap != null);
+		//fail("Not yet implemented");
+	}
+
+}
