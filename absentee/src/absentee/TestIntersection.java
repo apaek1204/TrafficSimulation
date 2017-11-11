3f9c1f626ea4dc7b515354d0b98353481272540e
quyaoxian <quyaoxian@gmail.com>
2017-11-10 19:08:44 -0500

    add unit test for intersection

---
+++ absentee/src/absentee/TestIntersection.java
@@ -0,0 +1,35 @@
+package absentee;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import org.junit.Test;
+
+public class TestIntersection {
+
+	@Test
+	public void test() {
+		Stoplight s = new Stoplight(0, 0);
+		Road r = new Road(3, new Point(0, 0), new Point(0, 3), null);
+		Road[] roads = new Road[4];
+		roads[0] = r;
+		roads[1] = r;
+		roads[2] = r;
+		roads[3] = r;
+		Intersection i = new Intersection(roads, s, new Point(0, 4));
+		Car c = new BasicCar(0, 0, new Point(0, 0), new Point(0, 0), 0, new Point(0, 0));
+		//cannot enter because red light
+		assertFalse(i.canEnter(c));
+		
+		//change to green light
+		s.changeLight(1, 1);
+		assertTrue(i.canEnter(c));
+		
+		//enter car
+		i.Enter(c);
+		Car c2 = new BasicCar(0, 0, new Point(0, 0), new Point(0, 0), 0, new Point(0, 0));
+		assertFalse(i.canEnter(c2));
+	}
+
+}
