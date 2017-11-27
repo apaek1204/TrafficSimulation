86e5058180483b22feec59aca978b812990fefd0
apaek <apaek@LAPTOP-KJ995O2C>
2017-11-26 21:09:47 -0500

    add other files that were changed

--- absentee/src/absentee/BasicCar.java
+++ absentee/src/absentee/BasicCar.java
@@ -1,10 +1,11 @@
 package absentee;
 
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class BasicCar extends Car{
 
-	public BasicCar(double maxVel, double breakDis, Point cur, Point des, int dir, Point size) {
+	public BasicCar(double maxVel, double breakDis, Point cur, ArrayList<Point> des, int dir, Point size) {
 
 		super(maxVel, breakDis, cur, des, dir, size);
 		// TODO Auto-generated constructor stub
--- absentee/src/absentee/VehicleFactory.java
+++ absentee/src/absentee/VehicleFactory.java
@@ -1,10 +1,11 @@
 package absentee;
 
 import java.awt.Point;
+import java.util.ArrayList;
 
 public class VehicleFactory {
 
-	public Car createBasicCar(double maxVel, double breakDis, Point cur, Point des, int dir, Point s){
+	public Car createBasicCar(double maxVel, double breakDis, Point cur, ArrayList<Point> des, int dir, Point s){
 		Car tempCar = new BasicCar(maxVel, breakDis, cur, des, dir, s);
 		return tempCar;
 	}
