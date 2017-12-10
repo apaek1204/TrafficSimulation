f1191491aa6ca61a084d628917b607fc952c5c83
apaek <apaek@LAPTOP-KJ995O2C>
2017-12-10 16:36:55 -0500

    test road

--- absentee/src/absentee/TestRoad.java
+++ absentee/src/absentee/TestRoad.java
@@ -1,8 +1,9 @@
-/*package absentee;
+package absentee;
 
 import static org.junit.Assert.*;
 
 import java.awt.Point;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -11,16 +12,16 @@ public class TestRoad {
 
 	@Test
 	public void test() {
-		Point start = new Point(0,100);
+		Point2D.Double start = new Point2D.Double(0,100);
 		ArrayList <Point> endList = new ArrayList<Point>();
 		Point end = new Point(20,100);
 		endList.add(end);
 		Point size = new Point(20,14);
-		Road road1 = new Road(100, start, end);
+		Road road1 = new Road(100, new Point(0,100), end, 3);
 		Stoplight sl = new Stoplight(0, 1);
 		Intersection i = new Intersection(sl, end);
 		road1.addIntersection(i);
-		Car car1 = new BasicCar(5, 0, start, endList, 1, size); //maxVel, breakDis, cur, des, dir, s
+		Car car1 = new BasicCar(10, 5, start, endList, null, size); //maxVel, breakDis, cur, des, dir, s
 
 		if(road1.canEnter(size)){
 			road1.Enter(car1);
@@ -33,4 +34,3 @@ public class TestRoad {
 	}
 
 }
-*/
